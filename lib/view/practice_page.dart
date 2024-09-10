e3fb08e89a34226dab26d5ae6bb914fd5c9c4a34
rimuakter057 <rimu74688@gmail.com>
2024-09-10 13:08:21 +0600

    practice page

---
+++ lib/view/practice_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PracticePage extends StatelessWidget {
+  const PracticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Practice")),
+    );
+  }
+}
